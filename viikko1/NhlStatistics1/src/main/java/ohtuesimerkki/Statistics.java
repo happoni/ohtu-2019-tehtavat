d218f42cb0d8f7b4cf9f518f637a3e21aafe0101
Niko Häppölä <niko.happola@gmail.com>
2019-11-01 16:30:29 +0200

    tehtävä 15 done

--- viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -9,10 +9,9 @@ public class Statistics {
 
     private List<Player> players;
 
-    public Statistics() {
-        PlayerReader reader = new PlayerReader("https://nhlstatisticsforohtu.herokuapp.com/players.txt");
+    public Statistics(Reader reader) {
         players = reader.getPlayers();       
-    }
+    }    
 
     public Player search(String name) {
         for (Player player : players) {
